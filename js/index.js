b6edafb87fc5a1c21f07ffbe6fc09b0e906850cf
andi194 <58745968+andi194@users.noreply.github.com>
2020-09-05 13:37:30 +0100

    javascriptquiz2
    second version of javascript quiz

--- js/index.js
+++ js/index.js
@@ -1,48 +1,30 @@
 
-/**
- * hides intro and show question 1
- */
 function startQuiz() {
   document.getElementById("intro").style.display = "none";
   document.getElementById("question1").style.display = "inline-block";
 }
 
-/**
- * hides question 1 shows question2
- */
 function question2() {
   document.getElementById("question1").style.display = "none";
   document.getElementById("question2").style.display = "inline-block";
 }
 
-/**
- * hides question 2 shows question3
- */
 function question3() {
   document.getElementById("question2").style.display = "none";
   document.getElementById("question3").style.display = "inline-block";
 }
-
-/**
- * hides question 3 shows question4
- */
-function question4() {
-  document.getElementById("question3").style.display = "none";
-  document.getElementById("question4").style.display = "inline-block";
-}
-
-
-
+ 
+ document.getElementById("question3").style.display = "none";
+ document.getElementById("question4").style.display = "inline-block";
 
 function results() {
-  calculateResults()
-  document.getElementById("question6").style.display = "none";
-  document.getElementById("results").style.display = "inline-block";
+ calculateResults()
+ document.getElementById("question4").style.display = "none";
+ document.getElementById("results").style.display = "inline-block";
 }
 
-
 function calculateResults() {
-  const radios1 = document.querySelectorAll('input[name="quest1"]');
+  const radios1 = document.querySelectorAll('input[name="question1"]');
   let q1Value;
   for (const rb of radios1) {
       if (rb.checked) {
@@ -51,7 +33,7 @@ function calculateResults() {
       }
   }
   
-  const radios2 = document.querySelectorAll('input[name="quest2"]');
+  const radios2 = document.querySelectorAll('input[name="question2"]');
   let q2Value;
   for (const rb of radios2) {
       if (rb.checked) {
@@ -60,52 +42,35 @@ function calculateResults() {
       }
   }
 
-  
-  const radios2 = document.querySelectorAll('input[name="quest2"]');
-  let q2Value;
-  for (const rb of radios2) {
+  const radios3 = document.querySelectorAll('input[name="question3"]');
+  let q3Value;
+  for (const rb of radios3) {
       if (rb.checked) {
           q2Value = rb.value;
           break;
       }
   }
 
-  
-  const radios2 = document.querySelectorAll('input[name="quest3"]');
-  let q2Value;
-  for (const rb of radios2) {
+  const radios4 = document.querySelectorAll('input[name="question4"]');
+  let q4Value;
+  for (const rb of radios4) {
       if (rb.checked) {
           q2Value = rb.value;
           break;
       }
   }
 
+  let total = Number(q1Value) + Number(q2Value) + Number(q3Value) + Number(q4Value)
+  result = total / 4;
   
-  const radios2 = document.querySelectorAll('input[name="quest4"]');
-  let q2Value;
-  for (const rb of radios2) {
-      if (rb.checked) {
-          q2Value = rb.value;
-          break;
-      }
-  }
-
-
-
-
-
- let total = Number(q1Value) + Number(q2Value);
-  result = total / 2;
-  
-  if (result > 2) {
-    msg = "You answered mostly c you are snorlax!";
-  } else if (result >= 1.5) {
-    msg = "You answered mostly b, you are pikachu"!;
+  if (result > 8) {
+    msg = "You answered mostly c you are Jigglypuff!";
+  } else if (result > 4 ) {
+    msg = "You answered mostly b, you are Snorlax!";
   } else {
-    msg = "You answered mostly a, you are jigglypuff!";
+    msg = "You answered mostly a, you are Pikachu!";
   }
 
   document.getElementById("result").innerHTML = msg
 
 }
-
