0ee419dd142943e69a713bfac818243e1052bcc0
andi194 <58745968+andi194@users.noreply.github.com>
2020-09-10 10:14:57 +0100

    javascript 2
    javascript version 2

--- js/index.js
+++ js/index.js
@@ -13,17 +13,18 @@ function question3() {
   document.getElementById("question2").style.display = "none";
   document.getElementById("question3").style.display = "inline-block";
 }
- 
+
+ function question4(){
  document.getElementById("question3").style.display = "none";
  document.getElementById("question4").style.display = "inline-block";
-
+ }
 function results() {
  calculateResults()
  document.getElementById("question4").style.display = "none";
  document.getElementById("results").style.display = "inline-block";
 }
 
-function calculateResults() {
+
   const radios1 = document.querySelectorAll('input[name="question1"]');
   let q1Value;
   for (const rb of radios1) {
@@ -73,4 +74,40 @@ function calculateResults() {
 
   document.getElementById("result").innerHTML = msg
 
+
+function calculateResults() {}
+
+
+function hideElement(id) {
+ document.getElementById(id).style.display = "none";
 }
+
+
+function showElement(id){
+ document.getElementById("ifYes").style.display = "block";
+}
+
+
+function nextQuestion(questionNo){
+ questionId = "question" + questionNo
+ hideElement(questionId)
+ questiionNo ++
+ nextId =  "question" + questionNo
+}
+
+function getRadioVal(form, name) {
+ var val;
+
+ var radios = form.elements[name];
+ 
+ 
+ for (var i=0, len=radios.length; i<len; i++) {
+     if ( radios[i].checked ) { 
+         val = radios[i].value; 
+         break; 
+     }
+ }
+ return val; 
+}
+
+
